d122c0f94756063bc4af04f600939211cea7418a
Kevin Ruiz <kevinruiz@bigandbrown.com>
2015-07-07 11:54:50 -0400

    first commit

---
+++ src/CMAPI/Snap.php
@@ -0,0 +1,10 @@
+<?php namespace Lewispulse\Nubo\CameraManager;
+
+use Jyggen\Curl\Curl;
+use Jyggen\Curl\Request;
+
+class Nubo{
+    public function boom(){
+        return "pow surprise";
+    }
+}
